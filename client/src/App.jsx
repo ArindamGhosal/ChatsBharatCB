87b57866b884345a760a8e8238c022fc457b8606
Arindam <arindamstarghosal@gmail.com>
2024-03-29 23:28:27 +0530

    updated again8

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import Routes from "./Routes";
 
 
 function App() {
-  axios.defaults.baseURL = 'http://localhost:4040';
+  axios.defaults.baseURL = 'https://chats-bharat-api.vercel.app/';
   axios.defaults.withCredentials = true;
  
   return (
